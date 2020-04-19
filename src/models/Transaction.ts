005beef7db31246c2b391e10f24b893aa3864923
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-18 21:58:23 -0300

    Cria Listagem e criação de transactions

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('int')
   value: number;
 
+  @Column('uuid')
   category_id: string;
 
+  @Column('timestamp with time zone')
   created_at: Date;
 
+  @Column('timestamp with time zone')
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,34 @@
 // import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category_id: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_id,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
